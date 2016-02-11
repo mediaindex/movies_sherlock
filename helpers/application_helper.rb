dbfcd1d2390181afc5c4a931c8b9cd8901641c55
Kirill <kirillbrown@gmail.com>
2016-02-12 05:36:58 +0600

    Add movie poster as a photo

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -12,7 +12,11 @@ module ApplicationHelper
     output = ''
 
     jhash.each do |key, value|
-      output << "<tr><td>#{key}</td><td>#{value}</td></tr>"
+      if key == 'Poster'
+        output << "<tr><td>#{key}</td><td><img src=\"#{value}\" alt=\"Movie poster\" height=\"50%\" width=\"auto\"></td></tr>"
+      else
+        output << "<tr><td>#{key}</td><td>#{value}</td></tr>"
+      end
     end
 
     slim :result_page, :locals => { results: output }
