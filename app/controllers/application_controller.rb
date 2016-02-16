a708d119dc66315d799d0f489485c740feb014cd
Kirill <kirillbrown@gmail.com>
2016-02-16 22:36:25 +0600

    Show current users name in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
 
   def current_user
     if session[:user_id]
-      User.find(session[:user_id])
+      @user = User.find(session[:user_id])
     else
       nil
     end
