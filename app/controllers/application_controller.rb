128338698d48a2f183953ad5db2a4ba8f4760721
Kirill <kirillbrown@gmail.com>
2016-02-16 15:11:02 +0600

    Move 404 action to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < Sinatra::Base
 
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, '../views/') }
+
+  not_found do
+    status 404
+    slim :'404'
+  end
 end
