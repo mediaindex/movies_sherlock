cd5b22df6cf26f400c286aa01ddf22a90fb03651
Kirill <kirillbrown@gmail.com>
2016-02-18 23:18:12 +0600

    Add popular movie statistics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
       @users = User.count
       @all_movies = Movie.count
       @unique_movies = Movie.uniq.count(:title)
-      @popular_movie = Movie.group(:title).order('title DESC').limit(7).count(:id)
+      @popular_movie = Movie.group(:title).order('id DESC').limit(5).count(:id)
       slim :'users/index'
     else
       redirect("/auth/log_in")
