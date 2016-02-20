ca05157a832de1b8f03cc6ab4fdb436ad3f2c64e
Kirill <kirillbrown@gmail.com>
2016-02-20 13:04:56 +0600

    Move logic from heplers, refactor code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   get '/:id' do
     if current_user
-      begin
-        @user = User.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-        nil
-      end
-      site_statistics
-      user_statistics
+      @users = User.count
+      @all_movies = Movie.count
+      @unique_movies = Movie.uniq.count(:title)
+      @popular_movie = Movie.group(:title).count.sort_by { |_key, values| - values}.first(5)
+      @my_movies = @user.movies.count
+      @movies_search = Movie.user_search(current_user).sum(:search_count)
       slim :'users/index'
     else
       redirect("/auth/log_in")
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+  scope :user_search, ->(user) { where(user_id: user.id) }
   belongs_to :user
   validates :title, presence: true
 end
